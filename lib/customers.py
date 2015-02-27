77d4cb1f604cfabc025ff02a3b1c011b69241d1f
David Link <dvlink@gmail.com>
2015-02-27 00:40:28 -0500

    Customers Module - methods working

--- lib/customers.py
+++ lib/customers.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 from vlib import db
 from vlib.datatable import DataTable
@@ -14,25 +15,30 @@ class Customers(object):
     def add(self, record):
         record['created'] = datetime.now()
         try:
-            return self.dt.insertRow(record)
+            customer_id = self.dt.insertRow(record)
+            return Customer(customer_id)
         except Exception, e:
             raise CustomersError(e)
+
 
 class CustomerNotFound(Exception): pass
 
 class Customer(object):
     '''Preside over a single Customer Record'''
 
-    def __init__(self, email):
+    def __init__(self, identifier):
+        '''Identifier may be customer_id or email'''
+        if isinstance(identifier, (int, long)):
+            filter = 'id = %s' % identifier
+        else:
+            filter = 'email = "%s"' % identifier
         self.db = db.getInstance()
-        sql = 'select * from customers where email = "%s"' % email
+        sql = 'select * from customers where %s' % filter
         results = self.db.query(sql)
         if not results:
-            raise CustomerNotFound()
+            raise CustomerNotFound('%s not found' % filter)
         self.data = results[0]
         self.__dict__.update(self.data)
 
     def __repr__(self):
-        return self.data
-C=Customers()
-r = C.add({'email': 'mud', 'type_id': 1})
+        return str(self.data)
--- tests/tests.py
+++ tests/tests.py
@@ -6,9 +6,13 @@ from customers import Customer
 EMAIL = 'ut.nisi.a@orciUt.org'
 
 class CustomerTest(unittest.TestCase):
-    def test_getCustomer(self):
+    def test_getCustomer_byEmail(self):
         customer = Customer(EMAIL)
         self.assertEqual(customer.email, EMAIL)
+
+    def test_getCustomer_byId(self):
+        customer = Customer(1)
+        self.assertEqual(customer.id, 1)
 
 if __name__ == '__main__':
     unittest.main()

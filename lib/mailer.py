3937d52324ef8850095b106b5f19457875a9b751
David Link <dvlink@gmail.com>
2015-03-23 22:13:56 -0400

    Mailer: added bcc arg

--- lib/mailer.py
+++ lib/mailer.py
@@ -2,7 +2,7 @@ import smtplib
 import email.utils
 from email.mime.text import MIMEText
 
-def send_mail(to_name, to_addr, from_name, from_addr, subject, html_body):
+def send_mail(to_name, to_addr, from_name, from_addr, bcc, subject, html_body):
     
     # Create the message
     msg = MIMEText(html_body, 'html')
@@ -13,13 +13,12 @@ def send_mail(to_name, to_addr, from_name, from_addr, subject, html_body):
     server = smtplib.SMTP('localhost')
     #server.set_debuglevel(True)
     try:
-        server.sendmail(from_addr, [to_addr], msg.as_string())
+        server.sendmail(from_addr, [to_addr, bcc], msg.as_string())
     finally:
         server.quit()
 
 def test():
-    html = '<h1>Green Test</h1><p>this is a para</p>'
+    html = '<h1>Green black</h1><p>this is a para</p>'
     send_mail('David Link', '<dvlink@gmail.com>',
-              'Crowfly', 'info@crowfly.net',
-              'Test pink', html)
-
+              'Crowfly', 'info@crowfly.net', 'dvlink@yahoo.com',
+              'Test brown', html)
